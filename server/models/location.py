49a376227b8d9a9234ca7236c6a1093b0f7afd1a
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-10-02 23:12:23 +0300

    update relationship

--- server/models/location.py
+++ server/models/location.py
@@ -7,4 +7,5 @@ class Location(db.Model):
     description = db.Column(db.String(200))
     image_url = db.Column(db.String(200))
 
+    #one to many relationship with destination
     destinations = db.relationship('Destination', back_populates='location')
