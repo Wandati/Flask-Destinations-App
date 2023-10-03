50cc0fdf7d42bf3bf5c4018d7dac08cf2fe99df4
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-10-03 10:57:03 +0300

    mapped destination columns and relationships

--- server/models/destination.py
+++ server/models/destination.py
@@ -1,15 +1,12 @@
 from .dbconfig import db
 
 class Destination(db.Model):
-     
-    id = db.Column(db.Integer, primary_key=True )
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    description = db.Column(db.Text)
-    image_url = db.Column(db.String)
-    location_id = db.Column(db.Integer, ForeignKey=('location.id'), nullable =False)
-    location=db.relation('Location', back_populates='destinations')
-    reviews = db.relationship('Review', back_populates='destination', lazy=True)
-
+    description = db.Column(db.String)
+    image_url = db.Column(db.String(255))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
+    reviews = db.relationship('ReviewDestination',back_populates='destination')
 
 
 
