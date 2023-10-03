a54ecd1edf8e53f18cabcd0663e570824bc17bc1
+ <lucy.njoroge1@student.moringaschool.com>
2023-10-03 07:44:20 +0300

    User Model

--- server/models/user.py
+++ server/models/user.py
@@ -1,5 +1,17 @@
 from .dbconfig import db 
-class User(db.Model):
-    pass
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
 
-    
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    password = Column(String(120), nullable=False)
+
+    # one-to-many relationship with reviews
+    reviews = relationship('Review', backref='user', lazy=True)
+
+    # many-to-many relationship with destinations via reviews
+    destinations = relationship('Destination', secondary='reviews', back_populates='users')
