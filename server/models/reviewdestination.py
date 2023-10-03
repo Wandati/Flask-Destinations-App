b23702de209b22a580f563c01e05e384c3b3ea26
Wandati <Petersimon801@gmail.com>
2023-10-03 11:37:39 +0300

    db

---
+++ server/models/reviewdestination.py
@@ -0,0 +1,9 @@
+from .dbconfig import db
+
+class ReviewDestination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    review_id = db.Column(db.Integer, db.ForeignKey('review.id'), nullable=False)
+    destination_id= db.Column(db.Integer, db.ForeignKey('destination.id'), nullable=False)
+    
+    review= db.relationship('Review',back_populates='destinations')
+    destination= db.relationship('Destination',back_populates='reviews')
