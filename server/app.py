36f780d51fc3e0c0f2bfc6abf9da30ab7e961e80
Wandati <Petersimon801@gmail.com>
2023-10-03 13:13:18 +0300

    authentication,signup,login

--- server/app.py
+++ server/app.py
@@ -5,6 +5,57 @@ from models.reviewdestination import ReviewDestination
 from models.destination import Destination
 from models.user import User
 from flask_restful import Resource
+from flask import jsonify,session,request
 
-if __name__ == "__main__":
-    app.run(port=5500,debug=True)
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data["username"]
+        email= data["email"]
+        password = data["password"]
+        existing_user=User.query.filter_by(username=username).first()
+        if existing_user:
+            return {"error":"Username Already Exists"},401
+        new_user = User(username=username,email=email)
+        new_user.password_hash = password
+        db.session.add(new_user)
+        db.session.commit()
+        session["user_id"] = new_user.id
+        return {},201
+class Login(Resource):
+    def post(self):
+        data= request.get_json()
+        username = data["username"]
+        password = data["password"]
+        user=User.query.filter_by(username=username).first()
+        
+        if user and user.authenticate(password):
+            session["user_id"] = user.id
+            return {},200
+        return {"error":"Invalid Username or Password"},401
+
+class CheckSession(Resource):
+    def get(self):
+       user = User.query.filter(User.id == session.get('user_id')).first()
+       if user:
+           return {},200
+       else:
+           return {'message': '401: Not Authorized'}, 401
+       
+class Logout(Resource):
+    def delete(self):
+        if "user_id" in session:
+            session["user_id"] = None
+            return {},204
+        return {"error":"Unauthorized action"},401
+            
+    
+api.add_resource(Login,"/login",endpoint="login")
+api.add_resource(Logout,"/logout",endpoint="logout")
+api.add_resource(Signup,"/signup",endpoint="signup")
+api.add_resource(CheckSession,"/checksession",endpoint="checksession")
+
+
+if __name__=="__main__":
+    app.run(port=5555,debug=True)
