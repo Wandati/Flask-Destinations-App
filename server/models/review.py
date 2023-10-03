56b36dc684f9db9447030c1b09aa638fa6d2db04
Wandati <Petersimon801@gmail.com>
2023-10-03 14:27:12 +0300

    validation

--- server/models/review.py
+++ server/models/review.py
@@ -1,5 +1,5 @@
 from .dbconfig import db
-
+from sqlalchemy.orm import validates
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer)
@@ -8,3 +8,11 @@ class Review(db.Model):
     
     # destination_id = db.Column(db.Integer,db.ForeignKey('destination.id'))
     destinations = db.relationship('ReviewDestination',back_populates='review')
+    
+    @validates("rating")
+    def validates_rating(self,key,rating):
+        if not (1<= rating <=10):
+            raise ValueError("Rating should be between 1 and 10.")
+        return rating
+    def __repr__(self):
+        return f"Review comment: {self.comment} ,rating: {self.rating} by User: {self.user_id}"
