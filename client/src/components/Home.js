977bc3d7923ac4b8398b8cbd4d6bf106c4a68874
Marvin Wandati <134693858+Wandati@users.noreply.github.com>
2023-11-24 14:09:25 +0300

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
   const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-    fetch("/locations")
+    fetch("https://destinations-server-app.onrender.com/locations")
       .then((res) => res.json())
       .then((data) => setLocations(data));
   }, []);
